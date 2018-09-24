8363d092497205e5be96c9e02d836ccab48795ef
derf-dog <38248717+derf-dog@users.noreply.github.com>
2018-09-24 21:31:02 +0930

    Update add.php

--- add.php
+++ add.php
@@ -137,6 +137,12 @@ if(isset($_POST['submit'])){
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/wheelers.css">
 		
+		<!-- link Jquery, Bootstrap -->
+		<script src="js/jquery-3.3.1.slim.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		
+		
+		
 		<title>Wheelers & Deelers</title>
 		
 		<script>
@@ -163,7 +169,33 @@ if(isset($_POST['submit'])){
 	</head>
 		
 	<body>
-
+		<!-- Header/navigation bar div -->
+		<!-- https://getbootstrap.com/docs/4.0/components/navbar/? -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<!-- branding logo image -->
+			<a class="navbar-brand" href="#">
+				<img src="images/robber-clipart-car-5.png" class="nav-logo">
+			</a>
+			<!-- collapse navigation to hamburger on small/mobile screens -->
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			
+			<!-- navigation bar -->
+			<div class="collapse navbar-collapse" id="navbarSupportedContent"> 
+				<ul class="navbar-nav mr-auto mx-auto">
+					<li class="nav-item active"><a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a></li>
+					<li class="nav-item"><a class="nav-link" href="#">About</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Register</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+				</ul>
+				<!-- login button -->
+				<div>
+					<a class="btn btn-sm btn-outline-secondary" type="button" href="add.html">Login</a>
+					<a href="#">Register</a>
+				</div>
+			</div>
+		</nav>
 		
 		<div class="main">
 			<h1>Wheelers & Dealers</h1>
@@ -192,7 +224,6 @@ if(isset($_POST['submit'])){
 									document.getElementById("year").innerHTML = yearRangeStr;
 								</script>
 							</datalist>
-						</input>
 					</div>
 				</div>
 				
@@ -404,11 +435,13 @@ if(isset($_POST['submit'])){
 			</form>
 		</div>	
 		
-
+		<footer class="page-footer fixed-bottom">
+			<div class="container-fluid text-left">
+				<a href="#">Privacy Policy</a>
+				<a href="#">Contact</a>
+				<a href="#">Logout</a>
+			</div>
+		</footer>
 		
-		<!-- link Jquery, Bootstrap, and Popper.js -->
-		<script src="js/jquery-3.3.1.slim.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-
 	</body>
 </html>
