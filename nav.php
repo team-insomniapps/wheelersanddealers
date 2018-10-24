ec1832de68f21e999477e05756eb2651335b66e1
derf-dog <38248717+derf-dog@users.noreply.github.com>
2018-10-24 11:12:43 +1030

    Update nav.php

--- nav.php
+++ nav.php
@@ -6,18 +6,18 @@
 	if(isset($_SESSION['loginID']))
 
 	{
-	?>
-<nav class="navbar navbar-expand-lg">
-	<!-- branding logo image -->
-	<a class="navbar-brand" href="index.php">
-	<img src="images/wdlogo.svg" class="navLogo d-inline-block align-top">
-	</a>
-	<!-- collapse navigation to hamburger on small/mobile screens -->
-	<button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="toggle navigation">
-	<span class="navbar-toggler-icon"></span>
-	</button>
+?>
+		<nav class="navbar navbar-expand-lg">
+			<!-- branding logo image -->
+			<a class="navbar-brand" href="index.php">
+			<img src="images/wdlogo.svg" class="navLogo d-inline-block align-top">
+			</a>
+			<!-- collapse navigation to hamburger on small/mobile screens -->
+			<button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
 
-  <!-- navigation bar -->
+			<!-- navigation bar -->
 			<div class="collapse navbar-collapse" id="navbarSupportedContent"> 
 				<ul class="navbar-nav mr-auto mx-auto">
 					<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
@@ -25,25 +25,22 @@
 					<li class="nav-item"><a class="nav-link" href="search.php">Search</a></li>
 					<li class="nav-item"><a class="nav-link" href="match.php">Matches</a></li>
 					<li class="nav-item"><a class="nav-link" href="inventory.php">Inventory</a></li>
-					
-					
+
+
 					<!-- <li class="nav-item"><a class="nav-link" href="#">Messages</a></li>
 					<li class="nav-item"><a class="nav-link" href="#">Account & Settings</a></li>
 					-->
 					<li class="nav-item"><a class="nav-link" href="about.php">Help</a></li>
 				</ul>
-					
-					
-				</ul>
+
 				<!-- login/logout button -->
 				<div>
 					<a class="logBtn btn btn-sm btn-outline-secondary"  href="logout.php">Logout</a>
 
 				</div>
 			</div>
-	</div>
 
-</nav>
+		</nav>
 
 <?php 
 	}
