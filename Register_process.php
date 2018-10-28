002219a54f5fb453bb396b545e8cf538b9bfb2af
Jason <42905375+JasonLane@users.noreply.github.com>
2018-10-28 16:08:54 +1100

    Add files via upload
    Might need to change the output on the index.php it is under <div>results</div>

---
+++ Register_process.php
@@ -0,0 +1,86 @@
+<?php 
+
+	// posted variables
+	$name = $_POST['postname'];
+	$email = $_POST['postemail'];
+	$phone = $_POST['postphone'];
+	$passwords = $_POST['postpass'];
+	$con_pass = $_POST['postconpass'];
+	$dealer = $_POST['postdn'];
+	$dealerloc = $_POST['postdl'];
+	
+	echo $name;
+	echo $email;
+	echo $phone;
+	echo $passwords;
+	echo $con_pass;
+	echo $dealer;
+	echo $dealerloc;
+
+	// database info
+	$servername = "localhost";
+	$dbname = "Wheelersanddealers";
+	$dsn = "mysql:host=$servername;dbname=$dbname";
+	
+	// connect to database
+	$username = "efftwelv_andrew";
+	$password = "Andrew1000";
+	 
+	if($dealer == "") {
+		
+		$permission = 1;
+		$conn = mysqli_connect($servername,$username,$password,$dbname);
+		
+		// set autocommit to off 
+		mysqli_autocommit($conn, FALSE);
+	
+		mysqli_query($conn, "INSERT INTO users(`customer_login`, `customer_email`,
+		`customer_pass`,`customer_phone`, `access_level`) VALUES ('{$name}', '{$email}', '{$passwords}',
+		'{$phone}', '{$permission}')");
+	
+		//commit transaction 
+		if (!mysqli_commit($conn)) {
+			echo "<script>alert('Transaction failed: ')</script>";
+		}
+		
+	} else {
+	 
+		$permission = 2;
+		$conn = mysqli_connect($servername,$username,$password,$dbname);
+		
+		// set autocommit to off 
+		mysqli_autocommit($conn, FALSE);
+	
+		mysqli_query($conn, "INSERT INTO users(`customer_login`, `customer_email`,
+		`customer_pass`,`customer_phone`, `dealer_name`, `dealer_location`, `access_level`) VALUES ('{$name}', '{$email}', '{$passwords}',
+		'{$phone}', '{$dealer}', '{$dealerloc}','{$permission}')");
+	
+		//commit transaction 
+		if (!mysqli_commit($conn)) {
+			echo "<script>alert('Transaction failed: ')</script>";
+		}
+	}
+	/*	
+	// send all information off to the database.
+		try{
+			
+			
+
+			// set autocommit to off 
+			mysqli_autocommit($conn, FALSE);
+
+			
+
+
+			//commit transaction 
+			if (!mysqli_commit($conn)) {
+					echo "<script>alert('Transaction failed: ')</script>";
+			}
+			
+		} catch(Exception $e ){
+			echo "<script>alert('Connection failed: ')</script>";
+		}
+	} 
+	
+	*/
+?>
--- index.php
+++ index.php
@@ -16,88 +16,16 @@
 		<link rel="stylesheet" href="css/wheelers.css">
 		
 		<!-- link Jquery, Bootstrap, and Popper.js -->
-		<script src="js/jquery-3.3.1.slim.min.js"></script>
+		<script type="text/javascript" src="js/jquery-3.3.1.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		
 		<!-- Google fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto" rel="stylesheet">
-
+		
 		<title>Wheelers & Deelers</title>
-		
-		<script>
-
-			var username;
-			var password;
-			var personalname;
-			//var phone;
-			var poolData;
-			
-				
-		  function registerButton() {
-			
-			personalnamename =  document.getElementById("personalnameRegister").value;	
-			username = document.getElementById("emailInputRegister").value;
-			// for phone verification later
-			//phone = document.getElementById("phoneInputRegister").value;
-			
-			if (document.getElementById("passwordInputRegister").value != document.getElementById("confirmationpassword").value) {
-				alert("Passwords Do Not Match!")
-				throw "Passwords Do Not Match!"
-			} else {
-				password =  document.getElementById("passwordInputRegister").value;	
-			}
-			
-			poolData = {
-					UserPoolId :'ap-southeast-2_lmX0a1XIT', // Your user pool id here
-					ClientId : '5qts5daiuu5m8d82c443mk26jf', // Your client id here
-				};		
-			var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-			var attributeList = [];
-			
-			var dataEmail = {
-				Name : 'email', 
-				Value : username, //get from form field
-			};
-			
-			var dataPersonalName = {
-				Name : 'name', 
-				Value : personalname, //get from form field
-			};
-			
-			/*
-			var dataPhoneNumber = {
-				Name : 'phone_number', 
-				Value : phone, //get from form field
-			};
-			*/
-
-			var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-			var attributePersonalName = new AmazonCognitoIdentity.CognitoUserAttribute(dataPersonalName);
-			//var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber);
-			
-			attributeList.push(attributeEmail);
-			attributeList.push(attributePersonalName);
-			//attributeList.push(attributePhoneNumber);
-			
-			userPool.signUp(username, password, attributeList, null, function(err, result){
-				if (err) {
-					alert(err.message || JSON.stringify(err));
-					return;
-				}
-				//cognitoUser = result.user;
-				//console.log('user name is ' + cognitoUser.getUsername());
-				//change elements of page
-				window.location = 'https://52.63.106.64/wheelersanddealers/index2.php';
-				//document.getElementById("titleheader").innerHTML = "Check your email for a verification link";
-			});
-		  }
-
-	</script>
-		
 
 	</head>
-		
+	
 	<!-- Header/navigation bar div -->
 	<!-- https://getbootstrap.com/docs/4.0/components/navbar/? -->
 	<nav class="navbar navbar-expand-lg">
@@ -135,9 +63,8 @@
 		</div>
 
 	</nav>
-		
-	<body>		
-		<div class="container">
+	<body>
+	<div class="container">
 			<div class="jumbotron">
 				<h1 class="display-4">Wheelers & Dealers</h1>
 				<p class="lead">Lets make deals on your wheels</p>
@@ -151,7 +78,7 @@
 				<a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
 			</div>
 			
-			<!-- Modal2 = register -->
+		<!-- Modal2 = register -->
 			<div id="myModal2" class="modal fade" role="dialog">
 				<div class="modal-dialog">
 
@@ -163,30 +90,134 @@
 						<button type="button" class="close" data-dismiss="modal">&times;</button>
 					</div>
 					<div class="modal-body">
-						<p>Username<input type="personalname" class="form-control" id="personalnameRegister" placeholder="Username" pattern=".*" required></p>
-						<p>Email<input type="email" class="form-control" id="emailInputRegister" placeholder="Email" pattern=".*" required></p>
-						<p>Phone<input type="phone" class="form-control" id="phoneInputRegister" placeholder="Phone" pattern=".*" required></p>
 						
-						<p>Password<input type="password" class="form-control" id="passwordInputRegister" placeholder="Password" pattern=".*" required></p>
-						<p>Confirm Password<input type="password" class="form-control" id="confirmationpassword" placeholder="Confirm Password" pattern=".*" required></p>
-	
+						<form>
+						<p>Username<input type="personalname" class="form-control" id="names" placeholder="Username"></p>
+						<p>Email<input type="personalemail" class="form-control" id="email" placeholder="Email" ></p>
+						<p>Phone<input type="personalphone" class="form-control" id="phone" placeholder="Phone" ></p>
+						
+						<p>Password<input type="password" class="form-control" id="password" placeholder="Password"></p>
+						<p>Confirm Password<input type="password" class="form-control" id="confirmpassword" placeholder="Confirm Password" ></p>
+						<hr>
+						<p>Dealers Name<input type="Dealername" class="form-control" id="dealername" placeholder="Dealers Name"></p>
+						<p>Dealers Location <input type="Dealerlocation" class="form-control" id="dealerlocation" placeholder="Dealers Location"></p>
 						<div class="modal-footer">
-							<button id="mainbutton" class="btn btn-lg btn-primary btn-block" type="button" onclick="registerButton()" >Register</button>
+							<button name="submit" class="btn btn-lg btn-primary btn-block" type="button" onclick="posts()"  >Register</button>
 							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 						</div>
+						
+						</form>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	
-	<footer class="page-footer">
+				
+		
+		<div id="results"></div>
+		
+	</div>
+		
+		<script type="text/javascript">
+		
+		var usernames;
+		var passwords;
+		var personalname;
+		//var phone;
+		var poolData;
+		
+		
+		function posts(){
+			 
+			// close model 
+			$('#myModal2').modal('hide');	
+			
+			// connect user to aws cognito for email adress validation
+			// phone validation is ready to be implemented.
+			personalnamename =  document.getElementById("names").value;	
+			usernames = document.getElementById("email").value;
+			// for phone verification later
+			//phone = document.getElementById("phoneInputRegister").value;
+			
+			if (document.getElementById("password").value != document.getElementById("confirmpassword").value) {
+				alert("Passwords Do Not Match!")
+				throw "Passwords Do Not Match!"
+			} else {
+				password =  document.getElementById("password").value;	
+			}
+			
+			poolData = {
+					UserPoolId :'ap-southeast-2_lmX0a1XIT', // Your user pool id here
+					ClientId : '5qts5daiuu5m8d82c443mk26jf', // Your client id here
+				};		
+			var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+			var attributeList = [];
+			
+			var dataEmail = {
+				Name : 'email', 
+				Value : username, //get from form field
+			};
+			
+			var dataPersonalName = {
+				Name : 'name', 
+				Value : personalname, //get from form field
+			};
+			
+			/*
+			var dataPhoneNumber = {
+				Name : 'phone_number', 
+				Value : phone, //get from form field
+			};
+			*/
+
+			var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+			var attributePersonalName = new AmazonCognitoIdentity.CognitoUserAttribute(dataPersonalName);
+			//var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber);
+			
+			attributeList.push(attributeEmail);
+			attributeList.push(attributePersonalName);
+			//attributeList.push(attributePhoneNumber);
+			
+			userPool.signUp(usernames, password, attributeList, null, function(err, result){
+				if (err) {
+					alert(err.message || JSON.stringify(err));
+					return;
+				}
+				cognitoUser = result.user;
+				console.log('user name is ' + cognitoUser.getUsername());
+				   
+				
+			});
+			
+			// post variables to database via index2.php
+			var username = $('#names').val();
+			var emails = $('#email').val();
+			var phones = $('#phone').val();
+			var pass = $('#password').val();
+			var conpass = $('#confirmpassword').val();
+			var dealname = $('#dealername').val();
+			var dealloc = $('#dealerlocation').val();
+			
+			$.post('Register_process.php', {postname:username, postemail:emails, postphone:phones,
+			postpass:pass, postconpass:conpass,postdn:dealname, postdl:dealloc},
+			function(data){
+				
+				// prints out post data -- need to be removed after data inserted into database as users.
+				$('#results').html(data);
+			});
+			
+		  }
+			
+		</script>
+		
+		<footer class="page-footer">
 		<div class="footerTxt container-fluid text-left">
 			<a class="footerTxt" href="#">Privacy Policy</a>
 			<a class="footerTxt" href="#">Contact</a>
 			<a class="footerTxt" href="#">Logout</a>
 		</div>
 	</footer>
+	
 	
  </body>  
 </html>
