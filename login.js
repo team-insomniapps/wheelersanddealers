bb9bfa6f25a268eb3967b66aba1edefcf982b3bb
Jason <42905375+JasonLane@users.noreply.github.com>
2018-10-29 13:45:56 +1100

    Files for AWS login and logout, register
    login.js
    loggedin.js
    auth.js
    logout.js

---
+++ login.js
@@ -0,0 +1,38 @@
+  function signInButton() {
+				
+	$('#myModal').modal('hide');	
+	
+	var authenticationData = {
+		Username : document.getElementById("uid").value,
+		Password : document.getElementById("pwd").value,
+	};
+	
+	var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+	
+	var poolData = {
+		UserPoolId : 'ap-southeast-2_lmX0a1XIT', // Your user pool id here
+		ClientId : '5qts5daiuu5m8d82c443mk26jf', // Your client id here
+	};
+	
+	var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+	
+	var userData = {
+		Username : document.getElementById("uid").value,
+		Pool : userPool,
+	};
+	
+	var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+	
+	cognitoUser.authenticateUser(authenticationDetails, {
+		onSuccess: function (result) {
+			var accessToken = result.getAccessToken().getJwtToken();
+			console.log(accessToken);	
+			location.reload();
+			
+		},
+
+		onFailure: function(err) {
+			alert(err.message || JSON.stringify(err));
+		},
+	});
+  }
